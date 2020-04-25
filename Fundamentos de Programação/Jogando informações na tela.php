d3f89b1cf0f37863b3801ac1d19ee49b21a8a319
Erik Alves <erikalvesprincipal@gmail.com>
2020-04-25 03:48:00 -0300

    v4.2

---
+++ Fundamentos de Programação/Jogando informações na tela.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+        <h1> Ola </h1>
+        <?php
+            $nome = "Erik";
+            $sobrenome = "Alves";
+            $idade = 17;
+
+            echo "Meu nome eh: ";
+            echo $nome." ".$sobrenome;
+            echo"e eu tenho";
+            echo $idade;
+            echo " anos";
+        ?>
+    </body>
+</html>
