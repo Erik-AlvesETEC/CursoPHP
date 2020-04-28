9a59bf899e22098a05162ef348f7ba2ae6f186b9
Erik Alves <erikalvesprincipal@gmail.com>
2020-04-28 18:47:02 -0300

    v1

---
+++ Javascript Intermediário/Eventos de Mouse.js
@@ -0,0 +1,26 @@
+function apertoMouse(){
+    console.log("APERTOU O MOUSE!");
+}
+function soltouMouse(){
+    console.log("SOLTOU O MOUSE!");
+}
+function passouMouse(){
+    console.log("MOUSE PASSOU EM CIMA!")
+}
+function saiumouse(){
+    console.log("MOUSE SAIU DE CIMA!");
+}
+function moveMouse(){
+    console.log("MOUSE FOI MOVIDO!");
+}
+function clicou(){
+    console.log("CLICOU!");
+}
+function botaoDireito(){
+    console.log("BOTÃO DIREITO");
+
+    return false;
+}
+function cliqueDuplo(){
+    console.log("CLICOU DUAS VEZES");
+}
